75dcbad290d76b12eb69d60a6e4bed2b9bad21f4
nirmalpraveen <nirmalpraveen2@gmail.com>
2021-07-06 14:08:35 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// Import stylesheets
-import './style.css';
+// Binary Search in Javascript
+let arr = [2, 1, 5, 3, 7, 4, 8, 6, 9, 10];
+let sortedArray = [...arr].sort((a, b) => a - b);
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function binarySearch(sortedArray, element) {
+  // This should return index of the element
+  // in binary search way
+  console.log(sortedArray, element);
+  let high = sortedArray.length - 1;
+  let low = 0;
+  let mid = Math.ceil((high + low) / 2);
+
+  while (sortedArray.length) {
+    if (sortedArray[mid] === element) {
+      console.log(sortedArray[mid]);
+    }
+  }
+}
+
+binarySearch(sortedArray, 7);
